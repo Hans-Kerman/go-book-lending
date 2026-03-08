1bd61b0bc3837dc5eaf01a17a8b815e266c000e8
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-03-08 22:00:36 +0800

    debug:修改接受前端传入的用户数据结构写了gorm嵌入结构体的重要错误

--- backend/types/user.go
+++ backend/types/user.go
@@ -1,11 +1,6 @@
 package types
 
-import (
-	"gorm.io/gorm"
-)
-
 type NewUser struct {
-	gorm.Model
 	UserName string `binding:"required,min=2,max=50" json:"user_name"`
 	Password string `binding:"required,min=6" json:"password"`
 }
