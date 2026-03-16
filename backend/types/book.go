b82571b4bc4a498a1d878cb7f4b1f068c8ec5a8a
ButterCatWSL <62628392+Hans-Kerman@users.noreply.github.com>
2026-03-16 11:39:59 +0800

    debug:删去[]byte的示例，防止swag出问题

--- backend/types/book.go
+++ backend/types/book.go
@@ -5,6 +5,6 @@ type NewBookInfo struct {
 	Author         string `json:"author" example:"Alan A. A. Donovan"`
 	ISBN           string `json:"isbn" binding:"required" example:"978-0134190440"`
 	Price          int    `json:"price" example:"40"`
-	CoverPicBase64 []byte `json:"cover_pic_base64" example:"base64_encoded_image_data"`
+	CoverPicBase64 []byte `json:"cover_pic_base64"`
 	Available      *int   `json:"available" example:"10"`
 }
